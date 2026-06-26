0d9c8e3b7408870556a072cd4a1022e544791869
prabhub95-pixel <prabhub95@gmail.com>
2026-06-26 14:26:44 +0530

    Update 01_kyc_overall_summary.sql

--- sql/01_kyc_overall_summary.sql
+++ sql/01_kyc_overall_summary.sql
@@ -1,7 +1,8 @@
-SELECT COUNT(*) AS total_customers,
+SELECT 
+    COUNT(*) AS total_customers,
     SUM(CASE WHEN kyc_status = 'Completed' THEN 1 ELSE 0 END) AS kyc_completed,
     SUM(CASE WHEN kyc_status = 'Pending' THEN 1 ELSE 0 END) AS kyc_pending,
     SUM(CASE WHEN kyc_status = 'Rejected' THEN 1 ELSE 0 END) AS kyc_rejected,
     SUM(CASE WHEN kyc_status = 'Expired' THEN 1 ELSE 0 END) AS kyc_expired,
     ROUND(SUM(CASE WHEN kyc_status = 'Completed' THEN 1 ELSE 0 END) * 100.0 / COUNT(*), 2) AS kyc_completion_rate_pct
-FROM bank_kyc_dataset;}
+FROM bank_kyc_dataset;
